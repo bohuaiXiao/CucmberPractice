6a887c278033927e9f17cddcdf422b43b7ad44a4
bohuai <tommy19900218@gmail.com>
2020-04-11 16:56:35 -0400

    Throw warning

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -12,7 +12,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue ="com/vytrack/step_definitions",
-        features = "src/test/resources/features"
+        features = "src/test/resources/features",
+        dryRun = true,
+        strict = true
 )
 public class CucumberRunner {
 
---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,30 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+/**
+ * @author:
+ * @create:
+ * @date:
+ */
+public class LoginStepDefinitions {
+
+    @Given("user is on the landing page")
+public void user_is_on_the_landing_page() {
+
+}
+
+
+   // Some other steps were also undefined:
+
+    @When("user logs is as a sales manager")
+    public void user_logs_is_as_a_sales_manager() {
+
+    }
+    @Then("user should verify that title is a Dashboard")
+    public void user_should_verify_that_title_is_a_Dashboard() {
+        
+    }
+}
