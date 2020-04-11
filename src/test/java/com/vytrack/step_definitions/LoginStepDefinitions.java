6bb5e985d31546758300fa66061395b56c2dc462
bohuai <tommy19900218@gmail.com>
2020-04-11 17:30:22 -0400

    LoginStep

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,8 +1,12 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 /**
  * @author:
@@ -10,9 +14,13 @@ import io.cucumber.java.en.When;
  * @date:
  */
 public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
 
     @Given("user is on the landing page")
 public void user_is_on_the_landing_page() {
+        System.out.println("Open login page");
+        String Url = ConfigurationReader.getProperty("qa3");
+        Driver.getDriver().get(Url);
 
 }
 
@@ -21,10 +29,14 @@ public void user_is_on_the_landing_page() {
 
     @When("user logs is as a sales manager")
     public void user_logs_is_as_a_sales_manager() {
+        System.out.println("Login as sales manager");
+        loginPage.login();
 
     }
     @Then("user should verify that title is a Dashboard")
     public void user_should_verify_that_title_is_a_Dashboard() {
-        
+        System.out.println("Verify that title is a Dashboard");
+        Assert.assertEquals("Dashboard",Driver.getDriver().getTitle());
+        Driver.closeDriver();
     }
 }
